7f8218559a003f1359868cc12a94c8e835a7a0f7
Alexander Pirsig <apirsig@web.de>
2013-04-02 20:25:17 +0200

    re-organize source

--- server.js
+++ server.js
@@ -1,60 +1,57 @@
-(function() {
-  var app, currentImg, drone, express, faye, imageSendingPaused, path, server, socket;
-  express = require("express");
-  faye = require("faye");
-  path = require("path");
-  drone = require("ar-drone").createClient();
-  drone.config('general:navdata_demo', 'TRUE');
-  app = express();
-  app.configure(function() {
-    app.set('port', process.env.PORT || 3001);
-    app.use(app.router);
-    app.use(express.static(path.join(__dirname, 'public')));
-    return app.use("/components", express.static(path.join(__dirname, 'components')));
-  });
-  server = require("http").createServer(app);
-  new faye.NodeAdapter({
-    mount: '/faye',
-    timeout: 45
-  }).attach(server);
-  socket = new faye.Client("http://localhost:" + (app.get("port")) + "/faye");
-  socket.subscribe("/drone/move", function(cmd) {
-    var _name;
-    console.log("move", cmd);
-    return typeof drone[_name = cmd.action] === "function" ? drone[_name](cmd.speed) : void 0;
-  });
-  socket.subscribe("/drone/animate", function(cmd) {
-    console.log('animate', cmd);
-    return drone.animate(cmd.action, cmd.duration);
-  });
-  socket.subscribe("/drone/drone", function(cmd) {
-    var _name;
-    console.log('drone command: ', cmd);
-    return typeof drone[_name = cmd.action] === "function" ? drone[_name]() : void 0;
-  });
-  server.listen(app.get("port"), function() {
-    return console.log("Express server listening on port " + app.get("port"));
-  });
-  currentImg = null;
-  drone.on('navdata', function(data) {
-    return socket.publish("/drone/navdata", data);
-  });
-  imageSendingPaused = false;
-  drone.createPngStream().on("data", function(frame) {
-    currentImg = frame;
-    if (imageSendingPaused) {
-      return;
-    }
-    socket.publish("/drone/image", "/image/" + (Math.random()));
-    imageSendingPaused = true;
-    return setTimeout((function() {
-      return imageSendingPaused = false;
-    }), 100);
-  });
-  app.get("/image/:id", function(req, res) {
-    res.writeHead(200, {
-      "Content-Type": "image/png"
+;(function() {
+
+    var app, currentImg, drone, express, imageSendingPaused, path, server, socket;
+    var express = require("express"),
+        path = require("path"),
+        app = express(),
+        nconf = require('nconf'),
+        config = nconf.file({file: __dirname+'/etc/config.json'}),
+        drone = require("ar-drone").createClient({ ip: config.get('app:droneip')});
+
+    drone.config('general:navdata_demo', 'TRUE');
+
+    app.configure(function(){
+
+        app.set('port', process.env.PORT || config.get('app:port'));
+        app.set('host', process.env.PORT || config.get('app:host'));
+        app.use("/libs", express.static(__dirname+ '/public/libs'));
+        app.use(express.static(__dirname+'/public'));
+        app.use(app.router);
     });
-    return res.end(currentImg, "binary");
-  });
+
+    var server = require("http").createServer(app);
+    var io = require('socket.io').listen(server);
+    io.set('destroy upgrade',false); // do not interfere with video stream websocket
+
+    var droneStream = require("dronestream").listen(server, { ip: config.get('app:droneip') });
+
+    io.sockets.on('connection',function(socket){
+
+        socket.on('/drone/move',function(cmd){
+            var _name;
+            console.log("move", cmd);
+            return typeof drone[_name = cmd.action] === "function" ? drone[_name](cmd.speed) : void 0;
+        });
+
+        socket.on('/drone/animate', function(cmd){
+            console.log('animate', cmd);
+            return drone.animate(cmd.action, cmd.duration);
+        });
+
+        socket.on("/drone/drone", function(cmd) {
+            var _name;
+            console.log('drone command: ', cmd);
+            return typeof drone[_name = cmd.action] === "function" ? drone[_name]() : void 0;
+        });
+
+
+        drone.on('navdata', function(data) {
+            socket.emit("/drone/navdata", data);
+        });
+    });
+
+    server.listen(app.get("port"),app.get("host"), function() {
+        return console.log("Express server listening on port " + app.get("port"));
+    });
+
 }).call(this);
